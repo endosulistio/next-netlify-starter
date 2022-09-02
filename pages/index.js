990b18777e6fd4f87ae46fcdab856d38b7c82e90
endo sulistio <39829514+endosulistio@users.noreply.github.com>
2022-09-02 12:07:55 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,7 @@ export default function Home() {
       <main>
         <Header title="Convert Pulsa" />
         <p className="description">
-    
-    
-Ada beberapa aplikasi convert pulsa jadi uang yang bisa memudahkan Anda menukarkan saldo pulsa menjadi uang digital.
-    <img src="https://tukar.co.id/assets/img/logo.png">
-    Uangku merupakan mobile wallet yang bisa Anda gunakan untuk melakukan pembayaran berbagai transaksi online. Aplikasi ini merupakan aplikasi dompet digital yang dikelola oleh PT Smartfren Telecom Tbk. Uangku juga bisa digunakan untuk mengkonversi pulsa jadi uang yang bisa ditransfer ke berbagai rekening bank dan dompet digital lainnya.  
-    Aplikasi konversi pulsa jadi uang yang juga banyak digunakan adalah ConvertPulsay. Aplikasi ini menggunakan metode transfer pulsa untuk sesama operator yang ditransfer baik ke dompet digital maupun ke ATM. <a href="https://tukar.co.id">Convert Pulsa</a> juga memiliki nilai rate yang tinggi sehingga bisa Anda jadikan pilihan untuk mengkonversi pulsa Anda menjadi uang.
-          Converin juga menjadi salah satu aplikasi konversi pulsa jadi uang yang banyak digunakan. Converin dibuat oleh developer JoonDev untuk memudahkan masyarakat dalam menggunakan pulsa dan menukarkannya menjadi uang tunai yang bisa digunakan untuk berbagai keperluan. Melalui aplikasi ini, Anda bisa menukarkan saldo pulsa Anda menjadi uang tunai yang bisa ditransfer ke rekening dan e-wallet seperti DANA, OVO, Gopay, dan ShopeePay. 
-
-    Itulah beberapa aplikasi convert pulsa jadi uang yang bisa membantu mengubah saldo pulsa menjadi uang tunai. Dengan adanya beberapa aplikasi ini, Anda tidak perlu perlu bingung untuk memanfaatkan pulsa Anda yang melimpah ruah.
+     Itulah beberapa aplikasi convert pulsa jadi uang yang bisa membantu mengubah saldo pulsa menjadi uang tunai. Dengan adanya beberapa aplikasi ini, Anda tidak perlu perlu bingung untuk memanfaatkan pulsa Anda yang melimpah ruah.
         </p>
       </main>
 
